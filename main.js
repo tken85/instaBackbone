67343b9b16d25a6b72f731569d15e1f23d86887a
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2015-11-16 22:23:58 -0500

    likes work

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ var instaPage ={
     instaPage.loadInstas();
   },
   initEvents: function(){
+    //submitting new InstaBam
     $('form').on('submit', function(event){
       event.preventDefault();
       var pic = $('input[name="pic"]').val();
@@ -29,25 +30,23 @@ var instaPage ={
       $('input[name="wordage"]').val("");
       instaPage.loadAgain();
     });
+    //updating likes
     $('section').on('click', 'button', function(event){
       event.preventDefault();
-      console.log("clicked");
+      $elId = $(this).closest('article').data('index');
+      var editingOne = instaPage.itemCollection.get($elId);
+      editingOne.set({likes: editingOne.get('likes')+1});
+      editingOne.save();
+      instaPage.loadAgain();
     });
   },
   loadInstas: function(){
-    instaPage.itemCollection.fetch().then(function(){
-      $('section').html('');
-     _.each(instaPage.itemCollection.models, function(currVal, idx, arr){
-        $('section').append('<article data-index="'+currVal.get('_id')+'"><img src="'+ currVal.get('pic_URL')+'"><p>'+currVal.get('text')+'</p><button type="button" class="btn btn-primary">Like</button>'+currVal.get('likes')+'</article>');
-        console.log(currVal);
-      });
-       console.log(instaPage.itemCollection);
-    });
+    instaPage.itemCollection.fetch().then(instaPage.loadAgain);
   },
   loadAgain: function(){
     $('section').html('');
     _.each(instaPage.itemCollection.models, function(currVal,idx,arr){
-      $('section').append('<article data-index="'+currVal.get('_id')+'"><img src="'+ currVal.get('pic_URL')+'"><p>'+currVal.get('text')+'</p><button type="button" class="btn btn-primary">Like</button>'+currVal.get('likes')+'</article>');
+      $('section').append('<article data-index="'+currVal.get('_id')+'"><img src="'+ currVal.get('pic_URL')+'"><p>'+currVal.get('text')+'</p><button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> Like</button><span class="likes">'+currVal.get('likes')+'</span></article>');
     });
   },
   itemCollection:"",
